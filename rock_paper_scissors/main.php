d965542abb58a20209d892f552837ed50c7b5613
Liva Kusina <liiva.kusina@gmail.com>
2023-03-19 15:39:44 +0200

    added first to three wins

--- rock_paper_scissors/main.php
+++ rock_paper_scissors/main.php
@@ -1,12 +1,13 @@
 <?php
 
+//Rock, paper, scissors game (with lizard and spock extension) where first to 3 wins;
+
 function makeElement(string $name, array $beats): stdClass{
     $element = new stdClass();
     $element->name = $name;
     $element->beats = $beats;
     return $element;
 }
-
 $rock = makeElement("rock", ["scissors", "lizard"]);
 $scissors = makeElement("scissors", ["paper", "lizard"]);
 $paper = makeElement("paper", ["rock", "spock"]);
@@ -20,12 +21,13 @@ $elements = [
     "spock"=>$spock,
     "lizard"=>$lizard
 ];
-
 $play = '';
-
+$computerWins = 0;
+$playerWins = 0;
+$round = 1;
 function play():string{
     global $play;
-    $value = strtolower(readline("Play? (yes/no) ". PHP_EOL));
+    $value = strtolower(readline("Start a new game? (yes/no) ". PHP_EOL));
 
     if($value === "no" || $value === "yes"){
         return $play = $value;
@@ -34,41 +36,57 @@ function play():string{
     return play();
 };
 
-echo "------------------------------------------" . PHP_EOL;
-echo "Game: Rock, paper, scissors, lizard, spock" . PHP_EOL;
-echo "------------------------------------------" . PHP_EOL;
+echo "******************************************" . PHP_EOL;
+echo "---Rock, paper, scissors, lizard, spock---" . PHP_EOL;
+echo "-------------First to 3 wins!-------------" .PHP_EOL;
+echo "******************************************" . PHP_EOL;
 
 play();
 
-while($play === "yes") {
+while($play === "yes" && $computerWins != 3 && $playerWins != 3) {
+    echo "              ***ROUND $round***". PHP_EOL;
     $playersChoice = strtolower(readline("Choose your element: "));
     $computersChoice = $elements[array_rand($elements)];
+    echo "__________________________________________". PHP_EOL;
 
     if (!array_key_exists($playersChoice, $elements)) {
         echo "Invalid element!". PHP_EOL;
-        play();
         continue;
     }
 
     $playersChoice = $elements[$playersChoice];
     echo "---Your choice: $playersChoice->name" . PHP_EOL;
     echo "---Computers choice: $computersChoice->name" . PHP_EOL;
-    echo "------------------------------------------" . PHP_EOL;
+    echo "__________________________________________" . PHP_EOL;
 
     if ($playersChoice === $computersChoice) {
+        $round++;
         echo "It's a tie!" . PHP_EOL;
-        echo "------------------------------------------". PHP_EOL;
-        play();
+        echo "__________________________________________". PHP_EOL;
         continue;
     }
 
     if (in_array($playersChoice->name, $computersChoice->beats)) {
-        echo "Computer wins!" . PHP_EOL;
+        $computerWins++;
+        echo "--Computer wins!" . PHP_EOL;
     } else {
-        echo "You win!" . PHP_EOL;
+        $playerWins++;
+        echo "--You win!" . PHP_EOL;
     }
-    echo "------------------------------------------". PHP_EOL;
-    play();
+    echo "__________________________________________". PHP_EOL;
+    echo "--Player: $playerWins\n--Computer: $computerWins".PHP_EOL;
+    echo "__________________________________________". PHP_EOL;
+
+    if ($playerWins === 3){
+        echo "*** Player has won the game! ***".PHP_EOL;
+        echo "__________________________________________". PHP_EOL;
+        play();
+    }elseif($computerWins === 3){
+        echo "*** Computer has won the game! ***".PHP_EOL;
+        echo "__________________________________________". PHP_EOL;
+        play();
+    }
+    $round++;
 }
 
 
