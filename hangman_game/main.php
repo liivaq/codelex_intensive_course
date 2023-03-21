793472e83a3ac3a3c98317cf83ffa3f95651f4a5
Liva Kusina <liiva.kusina@gmail.com>
2023-03-21 17:47:04 +0200

    added some checks, drawing for wrong guesses

--- hangman_game/main.php
+++ hangman_game/main.php
@@ -2,37 +2,59 @@
 
 // Game of Hangman - guess a random word, you lose after 3 wrong guesses;
 
-$words = ["falafel", "hiking", "pistachio", "programming", "lullaby"];
-$wordToGuess = $words[array_rand($words)];
-$hiddenWord = str_repeat("_", strlen($wordToGuess));
+$wrong1 = "     +-------+\n             |\n             |\n             |\n             |\n             |\n";
+$wrong2 = "     +-------+\n     |       |\n     0       |\n             |\n             |\n             |\n";
+$wrong3 = "     +-------+\n     |       |\n     0       |\n    /|\      |\n    / \      |\n             |\n";
 
-$guessArray = str_split($wordToGuess);
-$hiddenArray = str_split($hiddenWord);
+$words = ["falafel", "hiking", "pistachio", "programming", "lullaby"];
+$wordToGuess = str_split($words[array_rand($words)]);
+$hiddenWord = str_split(str_repeat("_", count($wordToGuess)));
 
 echo"Welcome to Hangman! Can you guess the word?".PHP_EOL;
-echo implode(" ", $hiddenArray).PHP_EOL;
+echo implode(" ", $hiddenWord).PHP_EOL;
 
 $wrongGuesses = 0;
-while($wrongGuesses < 4){
-
+while($wrongGuesses < 3){
     $guess = trim(strtolower(readline("Guess a letter: ")));
-    if(!in_array($guess, $guessArray)){
+
+    if (strlen($guess) != 1){
+        echo "Enter one letter!".PHP_EOL;
+        continue;
+    }
+
+    if(!in_array($guess, $wordToGuess)){
         $wrongGuesses++;
     }
 
-    $guessedIndexes = array_keys($guessArray, $guess);
-    foreach ($guessedIndexes as $index){
-        $hiddenArray[$index] = $guess;
+    if(in_array($guess, $hiddenWord)){
+        echo "You have already guessed this letter!".PHP_EOL;
+        continue;
     }
 
-    $result = implode(' ', $hiddenArray);
+    $letterPlacement = array_keys($wordToGuess, $guess);
+
+    foreach ($letterPlacement as $letter){
+        $hiddenWord[$letter] = $guess;
+    }
+
+    $result = implode(' ', $hiddenWord);
     echo $result.PHP_EOL;
     echo "Wrong guesses: $wrongGuesses".PHP_EOL;
 
-    if($result === implode(' ', $guessArray)){
-        echo "You have guessed the word correctly!".PHP_EOL;
-        exit;
-    };
+    switch ($wrongGuesses){
+        case 1:
+            echo "--Be careful, you're close to the gallows\n$wrong1";
+            break;
+        case 2:
+            echo "--Only one wrong guess allowed now...\n$wrong2";
+            break;
+        case 3:
+            echo "--You LOSE!\n$wrong3";
+    }
 
+    if($result === implode(' ', $wordToGuess)){
+        echo "--Congrats! You have guessed the word correctly!".PHP_EOL;
+        exit;
+    }
 }
 
